f1221353ea35e764c331c50a10fc79392e9e7754
Niclas Lind <niclas.lind@volvocars.com>
2019-11-14 10:00:00 +0100

    Added debug-serial-flag to this class

--- linbus-esp32/lib/Common/Common.hpp
+++ linbus-esp32/lib/Common/Common.hpp
@@ -2,6 +2,8 @@
 
 #include <cstdint>
 
-namespace {
-    static constexpr uint8_t UDP_TX_PACKET_MAX_SIZE_CUSTOM = 128;
-};
+namespace
+{
+constexpr bool LOG_TO_SERIAL = true;
+constexpr uint8_t UDP_TX_PACKET_MAX_SIZE_CUSTOM = 128;
+}; // namespace
