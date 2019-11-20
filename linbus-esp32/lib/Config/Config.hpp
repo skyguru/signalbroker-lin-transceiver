e4f665d0e84b03b7205c46c2093043e6237acfc6
Niclas Lind <niclas.lind@volvocars.com>
2019-11-20 16:53:21 +0100

    Cleaned up the code, grouped variables that belongs together to enums

--- linbus-esp32/lib/Config/Config.hpp
+++ linbus-esp32/lib/Config/Config.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
-#include <cstdint>
 #include <AsyncUDP.h>
+#include <array>
+
+#include "Records.hpp"
+#include "Common.hpp"
 
 class Config
 {
@@ -15,84 +18,118 @@ class Config
     };
 
 public:
-    Config(AsyncUDP &udp);
+    Config(Records &records);
+    /**
+    * "MASTER means that you need to reply to all ids, all except the master ids. For master id the data should be sent back.
+    * 
+    * "SLAVE" means that we send arbitration frames and master frames. Other responses should be sent back.
+    * 
+    * "UNDEFINED" means that you have choosed any mode yet
+    */
+    enum NodeModes : uint8_t
+    {
+        SLAVE = 0,
+        MASTER = 1,
+        UNDEFINED = 0xFF
+    };
+
+    enum HeartbeatModes : uint8_t
+    {
+        HEART_BEAT_TX_LIN = 1,
+        HEART_BEAT_RX_LIN = 2,
+        HEART_BEAT_TX_UDP = 3,
+        HEART_BEAT_RX_UDP = 4,
+        HEART_BEAT_SYNC_COUNT = 5,
+        HEART_BEAT_UNSYNCHED_PACKAGES = 6,
+        HEART_BEAT_SYNCHED_PACKAGES = 7
+    };
+
+    enum Offsets : uint8_t
+    {
+        HEADER_OFFSET = 0,
+        RIB_ID_OFFSET = 1,
+        HASH_HIGH_OFFSET = 2,
+        HASH_LOW_OFFSET = 3,
+        IDENTIFIER_OFFSET = 4,
+        PAYLOAD_SIZE_HIGH_OFFSET = 5,
+        PAYLOAD_SIZE_LOW_OFFSET = 6,
+        PAYLOAD_START_OFFSET = 7
+    };
 
 public:
-    void init();
-    void sendHeartbeat(int globalSynchedCount, int globalUnsynchedPackages, int globalSynchedPackeges);
-    // void parse_server_message();
-    void verifyConfig();
+    void init(uint8_t device_identifier);
+    void run();
+
     void requestConfigItem(uint8_t item);
-    // void send_logg_to_server(const char *message);
+
+    void incrementTxOverLin() { _txOverLin++; }
+    void incrementTxOverUdp() { _txOverUdp++; }
+    void incrementRxOverLin() { _rxOverLin++; }
+    void incrementRxOverUdp() { _rxOverUdp++; }
+
+    void setSynchedCount(int count) { _synchCount = count; }
+    void incrementSynchedPackages() { _synchedPackages++; }
+    void incrementUnsynchedPackages() { _unsynchedPackages++; }
+
+    IPAddress serverIp() { return ipserver; }
+    bool getLockIpAddress() { return _lockIpAddress; }
 
 public:
     uint8_t nodeMode() const { return _nodeMode; }
     uint8_t ribID() const { return _ribID; }
-
-public:
-    static constexpr int configPort() { return CONFIG_PORT; }
-    static constexpr int debugPortBase() { return DEBUG_PORT_BASE; }
-    static constexpr int heartBeatPeriod() { return HEARTBEAT_PERIOD; }
-    static constexpr uint8_t header() { return HEADER; }
-    static constexpr uint8_t hostPort() { return HOST_PORT; }
-    static constexpr uint8_t clientPort() { return CLIENT_PORT; }
-    static constexpr uint8_t modeMaster() { return MODE_MASTER; }
-    static constexpr uint8_t modeSlave() { return MODE_SLAVE; }
+    uint16_t hostPort() const { return _hostPort; }
+    uint16_t clientPort() const { return _clientPort; }
+    void log(const char *message);
 
 private:
-    uint8_t getRibID();
+    void sendHeartbeat();
+    void parseServerMessage();
+    void verifyConfig();
+    void logToServer(const char *message);
+    void clearCounters();
 
 private:
-    static constexpr int CONFIG_PORT = 4000;
-    static constexpr int DEBUG_PORT_BASE = 3000;
-    static constexpr int HEARTBEAT_PERIOD = 3000;
+    static constexpr int _configPort = 4000;
+    static constexpr int _heartbeatPeriod = 3000;
     static constexpr uint8_t HEADER = 0x03;
     static constexpr uint8_t HOST_PORT = (1 << 0);      // 1
     static constexpr uint8_t CLIENT_PORT = (1 << 1);    // 2
     static constexpr uint8_t MESSAGE_SIZES = (1 << 2);  // 4
     static constexpr uint8_t NODE_MODE = (1 << 3);      // 8
+    static constexpr uint8_t HEART_BEAT = (1 << 4);     // 16
     static constexpr uint8_t REASSIGN_RIBID = (1 << 5); // 32
     static constexpr uint8_t LOGGER = 0x60;             // 96
 
-    // Heartbeat
-    static constexpr uint8_t HEART_BEAT = (1 << 4);
-    static constexpr uint8_t HEART_BEAT_TX_LIN = 1;
-    static constexpr uint8_t HEART_BEAT_RX_LIN = 2;
-    static constexpr uint8_t HEART_BEAT_TX_UDP = 3;
-    static constexpr uint8_t HEART_BEAT_RX_UDP = 4;
-    static constexpr uint8_t HEART_BEAT_SYNC_COUNT = 5;
-    static constexpr uint8_t HEART_BEAT_UNSYNCHED_PACKAGES = 6;
-    static constexpr uint8_t HEART_BEAT_SYNCHED_PACKAGES = 7;
+    uint8_t _ribID; // This is calculated locally and part of the message header
+    AsyncUDP _udp;  // Udp-client
 
-    // LIN-Mode
-    static constexpr uint8_t MODE_SLAVE = 0;
-    static constexpr uint8_t MODE_MASTER = 1;
-    static constexpr uint8_t MODE_NONE = 0xFF;
-
-    // Offset
-    static constexpr uint8_t HEADER_OFFSET = 0;
-    static constexpr uint8_t RIB_ID_OFFSET = 1;
-    static constexpr uint8_t HASH_HIGH_OFFSET = 2;
-    static constexpr uint8_t HASH_LOW_OFFSET = 3;
-    static constexpr uint8_t IDENTIFIER_OFFSET = 4;
-    static constexpr uint8_t PAYLOAD_SIZE_HIGH_OFFSET = 5;
-    static constexpr uint8_t PAYLOAD_SIZE_LOW_OFFSET = 6;
-    static constexpr uint8_t PAYLOAD_START_OFFSET = 7;
-
-    uint8_t _ribID; //This is calculated locally and part of the message header
-    AsyncUDP _udp;
-
-    uint16_t _hostPort;
+    uint16_t _hostPort; // hostport to be used for udp-client in LinUpdGateway
     DoubleByte _hostPortHash;
+
     uint16_t _clientPort;
     DoubleByte _clientPortHash;
-    //size_t messageSizes;
+
     DoubleByte _messageSizesHash;
-    // "MODE_MASTER means that you need to reply to all ids, all except the master ids. For master id the data should be sent back.
-    // "MODE_SLAVE" means that we send arbitration frames and master frames. Other responses should be sent back.
-    uint8_t _nodeMode;
+
+    NodeModes _nodeMode = NodeModes::UNDEFINED;
     DoubleByte _nodeModeHash;
 
-    DoubleByte _lastHash; //Last server message had this config version (or config hash) and all config fields should match.
-    unsigned long _lastHeartbeat;
+    std::array<uint8_t, UDP_TX_PACKET_MAX_SIZE_CUSTOM> _packetBuffer{};
+    int _packetBufferLength;
+    bool _newData = false;
+
+    int _txOverLin = 0;
+    int _rxOverLin = 0;
+    int _txOverUdp = 0;
+    int _rxOverUdp = 0;
+    int _synchCount = 0;
+    int _synchedPackages = 0;
+    int _unsynchedPackages = 0;
+
+    IPAddress ipserver = {255, 255, 255, 255}; // Until the server adress is known we broadcast on the entire subnet
+    bool _lockIpAddress = false;               // Until we have recieved an ipaddress from the server, we have this set to false
+
+    DoubleByte _lastHash; // Last server message had this config version (or config hash) and all config fields should match.
+
+    Records *_records; // In here we store all out records
 };
