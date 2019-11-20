9ffd1d392fbadbb7c1d445fc6e71467f347cc0ad
Niclas Lind <niclas.lind@volvocars.com>
2019-11-20 16:58:15 +0100

    Moved out the functionsbodys to class instead of heaving them in the headerfile

--- linbus-esp32/lib/Record/Record.cpp
+++ linbus-esp32/lib/Record/Record.cpp
@@ -1,29 +1,69 @@
 #include "Record.hpp"
 
-Record::Record()
-{
-}
+#include <Arduino.h>
 
+Record::Record() {}
+
+/**
+ * @brief This defines the id of the frame in the record
+ * @param id: Id of the signal frame 
+ * */
 void Record::setId(uint8_t id)
 {
     _id = id;
 }
 
+/**
+ * @brief This defines the size of the frame in the record
+ * @param size: Size of the signal frame
+ * */
 void Record::setSize(uint8_t size)
 {
     _size = size;
 }
 
+/**
+ * @brief This defines if the record either is a master or a slave frame
+ * @param master: 0=slave, 1=master
+ * */
 void Record::setMaster(uint8_t master)
 {
     _master = master;
 }
 
-void Record::setWriteCache(uint8_t writeCache[])
-{
-}
-
+/**
+ * @brief Change state of cache valid
+ * @param cacheValid: True/false
+ * */
 void Record::setCacheValid(bool cacheValid)
 {
-    _cache_valid = cacheValid;
+    _cacheValid = cacheValid;
+}
+
+/**
+ * @brief Returns a byte of choosen index from the array
+ * @param index: Index of the array you want to get
+ * @return Byte of buffer
+ * */
+uint8_t Record::getWriteCacheByteByIndex(int index)
+{
+    return _writeChache.at(index);
+}
+
+/**
+ * @brief Return the entire writechache-buffer
+ * @return writecache buffer
+ * */
+std::array<uint8_t, 8> *Record::writeCache()
+{
+    return &_writeChache;
+}
+
+/**
+ * @brief Set the data of writechache buffer
+ * @param writeCache: array of data 
+ * */
+void Record::setWriteCache(std::array<uint8_t, 8> *writeCache)
+{
+    memcpy(&_writeChache, writeCache, writeCache->size());
 }
