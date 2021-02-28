64dadd9810074db94d12b1024b830c0e144f2d59
Lind, Niclas <niclas.lind@volvocars.com>
2021-02-28 15:18:46 +0100

    added invalid record data

--- lib/Common/Common.hpp
+++ lib/Common/Common.hpp
@@ -5,6 +5,7 @@
 namespace {
     constexpr bool LOG_TO_SERIAL = true;
     constexpr uint8_t UDP_TX_PACKET_MAX_SIZE_CUSTOM = 128u;
+    constexpr uint8_t INVALID_LIN_ID = 0xFFu;
 
     template<typename Input>
     uint8_t value(Input t) {
