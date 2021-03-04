a5b067d36cdbb97b4171ad6139825f1e036b292d
Lind, Niclas <niclas.lind@volvocars.com>
2021-03-04 17:29:00 +0100

    change to referenes instead of pointers

--- lib/Records/Records.cpp
+++ lib/Records/Records.cpp
@@ -9,23 +9,23 @@ Records::Records() {
  * @brief Returns the record on given index
  * @param index - index of the vector
  * */
-[[nodiscard]] Record *Records::getRecord(int index) {
-    Record *record = &_recordList.at(index);
-    return record;
-}
+// [[nodiscard]] Record *Records::getRecord(int index) {
+//     Record *record = &_recordList.at(index);
+//     return record;
+// }
 
 /**
  * @brief Returns a pointer to a record, if the id is valid
  * @param id - the id of the record you are searching for
  * */
-Record *Records::getRecordById(int id) {
-    for (auto &record : _recordList) {
-        if (record.id() == id) {
-            return &record;
-        }
-    }
-    return nullptr;
-}
+// Record *Records::getRecordById(int id) {
+//     for (auto &record : _recordList) {
+//         if (record.id() == id) {
+//             return &record;
+//         }
+//     }
+//     return nullptr;
+// }
 
 /**
  * @brief Add a record to the vector
@@ -38,6 +38,6 @@ void Records::add(Record record) {
 /**
  * @brief Returning the last element from the array of Records
  * */
-Record *Records::getLastElement() {
-    return &_recordList.back();
-}
+// Record *Records::getLastElement() {
+//     return &_recordList.back();
+// }
--- lib/Records/Records.hpp
+++ lib/Records/Records.hpp
@@ -10,29 +10,28 @@ class Records {
 public:
     Records();
 
-    Record *getRecord(int index);
+    // Record *getRecord(int index);
 
-
-    Record *getRecordById(int id);
+    // Record *getRecordById(int id);
 
     void add(Record record);
 
-    Record *getLastElement();
+    // Record *getLastElement();
 
     Record &getRecordRef(int index) {
         return _recordList.at(index);
     }
 
-//    Record &getRecordRefById(int id) {
-//        for (auto &record : _recordList) {
-//            if (record.id() == id) {
-//                return record;
-//            }
-//        }
-//
-//        // if the ID isn't found, return the invalid record
-//        return _recordList.back();
-//    }
+   Record &getRecordRefById(int id) {
+       for (auto &record : _recordList) {
+           if (record.id() == id) {
+               return record;
+           }
+       }
+
+       // if the ID isn't found, return the invalid record
+       return _recordList.back();
+   }
 
     Record &getLastElementByRef() {
         return _recordList.back();
