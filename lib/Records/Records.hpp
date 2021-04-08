a4f74ae58580b25eb1e1217c4931c9cc97441bcd
Lind, Niclas <niclas.lind@volvocars.com>
2021-04-08 11:36:23 +0200

    added comments

--- lib/Records/Records.hpp
+++ lib/Records/Records.hpp
@@ -8,33 +8,54 @@
 
 class Records {
 public:
-    Records();
 
-    // Record *getRecord(int index);
+    /**
+     * Storing records in a vector
+     */
+    Records() {
+        // Reserving some slots, to reduce the amount of times of reallocation
+        _recordList.reserve(30);
+    };
 
-    // Record *getRecordById(int id);
+    /**
+    * @brief Add a record to the vector
+    * @param record - Record you want to add
+    * */
+    void add(Record record) {
+        _recordList.push_back(record);
+    }
 
-    void add(Record record);
-
-    // Record *getLastElement();
-
+    /**
+     * @brief Get a record on a indicated index
+     * @param index - Index that you want to access in the vector
+     * @return The record on the index
+     */
     Record &getRecordRef(int index) {
         return _recordList.at(index);
     }
 
-   Record &getRecordRefById(int id) {
-       for (auto &record : _recordList) {
-           if (record.id() == id) {
-               return record;
-           }
-       }
+    /**
+     * @brief Get a record that match the indicated index
+     * @param id - The id of the record you want to access
+     * @return the record with the matching id if exists, otherwise return the invalid recrod
+     */
+    Record &getRecordRefById(int id) {
+        for (auto &record : _recordList) {
+            if (record.id() == id) {
+                return record;
+            }
+        }
 
-       // if the ID isn't found, return the invalid record
-       return _recordList.back();
-   }
-
-    Record &getLastElementByRef() {
+        // if the ID isn't found, return the invalid record
         return _recordList.back();
+    }
+
+    /**
+     * @brief Get all records
+     * @return the entire vector of records
+     */
+    std::vector<Record> getRecords() {
+        return _recordList;
     }
 
 private:
