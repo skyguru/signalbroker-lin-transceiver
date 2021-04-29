8de406719de6059f735dcc44477a1defaa096a97
Niclas <niclas.lind93@gmail.com>
2021-04-29 11:27:50 +0200

    deleted invalid_id

--- lib/Common/Common.hpp
+++ lib/Common/Common.hpp
@@ -2,13 +2,14 @@
 
 #include <cstdint>
 
-namespace {
+namespace
+{
     constexpr bool LOG_TO_SERIAL = true;
     constexpr uint8_t UDP_TX_PACKET_MAX_SIZE_CUSTOM = 128u;
-    constexpr uint8_t INVALID_LIN_ID = 0xFFu;
 
-    template<typename Input>
-    uint8_t value(Input t) {
+    template <typename Input>
+    uint8_t value(Input t)
+    {
         return static_cast<uint8_t>(t);
     }
 } // namespace
